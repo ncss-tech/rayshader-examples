06d7d39f21676534b383380cb1167f2b95bd6a42
John Hammerly <john.hammerly@gmail.com>
2022-09-26 12:13:08 -0500

    adding several examples
    adding examples of 3D profile visualization of OSDs and surrounding
    landscape and landforms of the Marshall series. Animations also
    included.

---
+++ OSD-pedon-loc-viz/build3d_profw.R
@@ -0,0 +1,50 @@
+build3dprofilew <- function(x, data) {
+  
+  f <- fetchOSD(x)
+  h <- horizons(f)
+  
+  h$thk <- (h$bottom - h$top)/100
+  
+  n <- length(h$hzID)
+  
+  fpc <- list()
+  
+  cl <- c()
+  
+  dps <- list()
+  fnl <- list()
+  
+  mtrx <- matrix(nrow = 4, ncol = 8)
+  
+  mtrd <- c()
+  
+  for (i in 1:n) {
+    
+    cl[i] <- c(h$soil_color[i])
+    
+    mtrd <- c(-50, h$top[i], -50, 50, 50, h$top[i], -50, 50, -50, h$bottom[i], -50, 50, 50, h$bottom[i], -50, 50, -50, h$top[i], 50, 50, 50, h$top[i], 50, 50, -50, h$bottom[i], 50, 50, 50, h$bottom[i], 50, 50)
+    
+    dps[i] <- list(matrix(data = mtrd, nrow = 4, ncol = 8))
+    
+    fpc[i] <- list(cube3d(color = cl[i]))
+    
+    fpc[[i]]$vb <- dps[[i]]
+    
+    cnt1 <- 0
+    
+    cnt2 <- 0
+    
+    fpc[[i]] <- rotate3d(obj = fpc[[i]], pi, 1, 0, 0)
+    
+    fpc[[i]] <- scale3d(obj = fpc[[i]], 50,100,50)
+    
+    fpc[[i]] <- translate3d(obj = fpc[[i]], x = cnt1, y = (data@data@max)+500, z = cnt2)
+    
+    fnw <- wire3d(fpc[[i]])
+    
+  }
+  
+  
+  
+  return(fnw)
+}
---
+++ OSD-pedon-loc-viz/build3d_profwhex.R
@@ -0,0 +1,66 @@
+build3dprofilewhex <- function(x, data) {
+  
+  f <- fetchOSD(x)
+  h <- horizons(f)
+  
+  h$thk <- (h$bottom - h$top)/100
+  
+  n <- length(h$hzID)
+  
+  fpc <- list()
+  
+  cl <- c()
+  
+  dps <- list()
+  fnl <- list()
+  
+  mtrx <- matrix(nrow = 4, ncol = 8)
+  
+  mtrd <- c()
+  
+  for (i in 1:n) {
+    
+    cl[i] <- c(h$soil_color[i])
+    
+    mtrd <- c(
+              0, h$top[i], -55, 50,
+              -40, h$top[i], -25, 50,
+              -40, h$top[i], 25, 50,
+              0, h$top[i], 55, 50,
+              40, h$top[i], 25, 50,
+              40, h$top[i], -25, 50,
+              0, h$bottom[i], -55, 50,
+              -40, h$bottom[i], -25, 50,
+              -40, h$bottom[i], 25, 50,
+              0, h$bottom[i], 55, 50,
+              40, h$bottom[i], 25, 50,
+              40, h$bottom[i], -25, 50
+    )
+    
+    dps[i] <- list(matrix(data = mtrd, nrow = 4, ncol = 12))
+    
+    fpc[i] <- list(extrude3d(c(0, -40, -40, 0, 40, 40), c(-55, -25, 25, 55, 25, -25)))
+    
+    fpc[[i]]$vb <- dps[[i]]
+    
+    fpc[[i]]$material$color <- cl[i]
+    
+    cnt1 <- 0
+    
+    cnt2 <- 0
+    
+    fpc[[i]] <- rotate3d(obj = fpc[[i]], pi, 1, 0, 0)
+    fpc[[i]] <- rotate3d(obj = fpc[[i]], pi/2, 0, 1, 0)
+    
+    fpc[[i]] <- scale3d(obj = fpc[[i]], 50,100,50)
+    
+    fpc[[i]] <- translate3d(obj = fpc[[i]], x = cnt1, y = (data@data@max)+500, z = cnt2)
+    
+    fnw <- dot3d(fpc[[i]])
+    
+  }
+  
+  
+  
+  return(fnw)
+}
